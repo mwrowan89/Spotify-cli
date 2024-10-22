c242d50e6f513eef7d4da35c810df14ba238b6c7
mwrowan89 <mwrowan89@gmail.com>
2024-10-22 10:28:25 -0600

    Added spotify api info

--- src/main/java/org/example/services/SpotifyService.java
+++ src/main/java/org/example/services/SpotifyService.java
@@ -1,4 +1,7 @@
 package org.example.services;
 
 public class SpotifyService {
+    private final String API_URL = "https://api.spotify.com/v1/search?q=artist:";
+    private final String CLIENT_ID = "6c372ce912574a7d9277e273ad0b5be2";
+    private final String CLIENT_SECRET = "d87baae9da33459aba91e165dfa180f8";
 }
