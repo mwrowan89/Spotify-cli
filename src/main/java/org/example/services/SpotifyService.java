68b4eb065d4fd2206e93527b4ee7d9d4126d5f25
mwrowan89 <mwrowan89@gmail.com>
2024-10-22 10:28:58 -0600

    Added spotify api buildr

--- src/main/java/org/example/services/SpotifyService.java
+++ src/main/java/org/example/services/SpotifyService.java
@@ -1,7 +1,18 @@
 package org.example.services;
 
+import se.michaelthelin.spotify.SpotifyApi;
+
+import java.net.URI;
+
 public class SpotifyService {
     private final String API_URL = "https://api.spotify.com/v1/search?q=artist:";
     private final String CLIENT_ID = "6c372ce912574a7d9277e273ad0b5be2";
     private final String CLIENT_SECRET = "d87baae9da33459aba91e165dfa180f8";
+
+    SpotifyApi spotifyApi = new SpotifyApi.Builder()
+            .setClientId(CLIENT_ID)
+            .setClientSecret(CLIENT_SECRET)
+            .setRedirectUri(URI.create("http://localhost:9000"))
+            .build();
+
 }
