33d21b68875d4e4819df6bd0af8f664a5009ef7d
mwrowan89 <mwrowan89@gmail.com>
2024-10-22 10:32:37 -0600

    Added cli scanner and prompts

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,17 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import org.example.services.SpotifyService;
+
+import java.util.Scanner;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello Spotify!" );
+    public static void main( String[] args ) {
+        System.out.println("Welcome to my Music App - let's find some music!");
+        Scanner scanner = new Scanner(System.in);
+        SpotifyService service = new SpotifyService();
+
+
     }
+
 }
